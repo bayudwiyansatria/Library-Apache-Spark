829ceeba2794cb0e3f254c4cb03f6a1b26bb1a4f
Bayu Dwiyan Satria <bayudwiyansatria@gmail.com>
2019-11-27 16:07:59 +0700

    Add Spark Test

---
+++ src/test/java/com/bayudwiyansatria/environment/apache/spark/SparkTest.java
@@ -0,0 +1,23 @@
+package com.bayudwiyansatria.environment.apache.spark;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SparkTest {
+	
+	@Test
+	public void Spark(){
+		JavaRDD<String> data = new SparkIO ().readData_String ( new Spark ().getSparkContext (), "src/main/resources/ruspini" );
+		
+	}
+	
+	@Test
+	public void Spark_ConvertRDD() {
+		JavaRDD<String> data = new SparkIO ().readData_String ( new Spark ().getSparkContext (), "src/main/resources/ruspini" );
+        double[][] newData = new SparkUtils().rdd_to_double (data);
+        new com.bayudwiyansatria.mat.Mat ().print ( newData );
+	}
+	
+}
